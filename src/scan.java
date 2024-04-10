c128fa42809e35a5f4cce746773a3a7bfe2ac7eb
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2024-04-10 11:09:18 +0200

    Hitta siffra i namn2

--- src/scan.java
+++
@@ -1,9 +0,0 @@
-
-public class scan {
-
-    public static String nextLine() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'nextLine'");
-    }
-
-}
